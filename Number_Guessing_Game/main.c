e94363feb1e8d9fafa41e55ab3e92ed4c294fe3b
cseaditya-000 <aditya3546raj@gmail.com>
2026-09-04 02:53:22 +0530

    -

--- Number_Guessing_Game/main.c
+++ Number_Guessing_Game/main.c
@@ -1,10 +1,97 @@
-#include <stdio.h, math.h, stdlib.h, time.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main (void)
+void true_game(int level, int random_number);
+
+int main(void)
 {
-    srand(time(null));
+    printf("Welcome to Number Guessing Game.\nIn this game, you will guess the random number chosen by computer.\nThere are three difficulty levels.\nYou must complete the prior to have acccess to the next level\n");
 
-    int random_number = (rand() % 100) + 1;
-    int guessed_number;
+    srand(time(NULL));
+
+    int choice;
+    int game = 0;
+    do{
+        int level = 1;
+        int random_number = (rand() % 100) + 1; // generates random number
+        printf("Computer has guessed its number.\n");
+        true_game(level, random_number);
+        game++;
+
+        printf("Now you have three choices.\n1.) Play again with same level of difficulty(aka level 1).\n2.) Play again with next level of difficult(aka level 2).\n3.) End the game.\n");
+        do{
+            printf("Please choose a valid option: ");
+            scanf("%d", &choice);
+        }while (choice != 1 && choice != 2 && choice != 3);
+
+        if (choice == 3){
+            printf("Congrats on completing %d games\n", game);
+            printf("Thanks for Playing.\n");
+            return 3;
+        }
+    }while (choice == 1); 
+    
+    do{
+        int level = 2;
+        int random_number = (rand() % 1000) + 1; // generates random number
+        printf("Computer has guessed its number.\n");
+        true_game(level, random_number);
+        game++;
+        
+        printf("Now you have three choices.\n1.) Play again with same level of difficulty(aka level 2).\n2.) Play again with next level of difficult(aka level 3).\n3.) End the game.\n");
+        do{
+            printf("Please choose a valid option: ");
+            scanf("%d", &choice);
+        }while (choice != 1 && choice != 2 && choice != 3);
+
+        if (choice == 3){
+            printf("Congrats on completing %d games\n", game);
+            printf("Thanks for Playing.\n");
+            return 3;
+        }
+    }while (choice == 1); 
+    
+    do{
+        int level = 3;
+        int random_number = (rand() % 10000) + 1; // generates random number
+        printf("Computer has guessed its number.\n");
+        true_game(level, random_number);
+        game++;
+        
+        printf("Now you have two choices.\n1.) Play again with same level of difficulty(aka level 3).\n2.) End the game.\n");
+        do{
+            printf("Please choose a valid option: ");
+            scanf("%d", &choice);
+        }while (choice != 1 && choice != 2);
+
+        if (choice == 2){
+            printf("Congrats on completing %d games\n\n", game);
+            printf("Thanks for Playing.\n");
+            return 2;
+        }
+
+    }while (choice == 1); 
+
     return 0;
 } 
+
+void true_game(int level, int random_number)
+{
+    int guessed_number = 0; // user number
+    int tries = 0;
+    while (guessed_number != random_number){
+        tries++;
+        printf("Guess the number between 1 and %d: ", (int)round(10 * (pow(10,level))));
+        scanf("%d", &guessed_number);
+        if (guessed_number < random_number){
+            printf("Try a larger number.\n");
+        }
+        if (guessed_number > random_number)
+        {
+            printf("Try a smaller number.\n");
+        }
+    }
+        printf("You guessed correct in %d tries.\n", tries);
+}
